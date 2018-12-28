8139f70136944189346dc3e853983dc432c02ac3
markoyee <markoyee@users.noreply.github.com>
2018-12-28 21:27:45 +0100

    program zapisuje pierwsze linijki z plików.txt do własnego pliku
    program zapisuje pierwsze linijki z plików.txt do własnego pliku

--- Przetwarzanie lasy.py
+++ Przetwarzanie lasy.py
@@ -4,7 +4,8 @@ import os
 #ustalam srodowisko (1. sciezka 2. srodowisko)
 print ('ustawiam sciezke dostepu do danych')
 path = r'D:\BDL 2018\BDL 2018\BDL'
-
+mass = r'D:\BDL 2018\BDL 2018\mass.txt'
+mass_plik = open(mass, "a")
 
 #wyswietlanie zawartosci folderu - funcja do tego jest z biblioteki os
 lista_plikow = os.listdir(path) #podgladam zawartosc
@@ -14,14 +15,14 @@ for obiekt in lista_plikow:
     #print path_x
     lista_plikow_wewnetrzna = os.listdir(path_x)
     for obiekt_zagniezdzony in lista_plikow_wewnetrzna:
+        # warunek zeby wczytywac pliki o okreslonej nazwie
         if obiekt_zagniezdzony.endswith('species.txt'):
-            #print (obiekt_zagniezdzony)
-            #print(path_x+'\\'+obiekt_zagniezdzony)
             plik = open(path_x+'\\'+obiekt_zagniezdzony)
-            tekst = plik.readline(30)
-            print tekst
+            tekst = plik.readline()
+            mass_plik.write(tekst)
             plik.close()
+            print(obiekt_zagniezdzony)
 
-
+mass_plik.close()
 
 print ('zakonczono' )
